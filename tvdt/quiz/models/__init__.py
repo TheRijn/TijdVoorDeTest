628bfa22f0eb11730c2843f2a316f5b26e8ace1c
Marijn Doeve <marijn@doeve.me>
2024-12-29 14:55:07 +0100

    Start of Symfony

--- tvdt/quiz/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .answer import Answer
-from .candidate import Candidate
-from .correction import Correction
-from .given_answer import GivenAnswer
-from .question import Question
-from .quiz import Quiz
-from .season import Season
